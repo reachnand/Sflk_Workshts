19fb5960b9c93fdfcf24e78ffa5d06708bf5f268
reachnand <anandoncloud99@gmail.com>
2024-07-16 10:00:05 +1000

    Create 01_01_Common_Dim_TIme.sql
    Script to create Dim_time for date dimension

---
+++ 01_01_Common_Dim_TIme.sql
@@ -0,0 +1,60 @@
+-- CREATE DATABASE IF NOT EXISTS LF_WH_DB;
+-- CREATE SCHEMA IF NOT EXISTS LF_WH_DB.COMMON;
+-- CREATE SCHEMA IF NOT EXISTS LF_WH_DB.MARKETING;
+-- USE DATABASE LF_WH_DB;
+-- USE SCHEMA COMMON;
+CREATE or REPLACE TABLE lf_wh_db.common.dim_date as
+WITH RECURSIVE CalendarDates AS (
+SELECT 
+    -- DATEFROMPARTS(YEAR(CURRENT_DATE) — 5, 1, 1) AS calendar_date
+    DATE_TRUNC('YEAR', DATEADD('YEAR', -5, CURRENT_DATE)) AS calendar_date
+UNION ALL
+SELECT DATEADD(DAY, 1, calendar_date)
+FROM CalendarDates
+WHERE calendar_date < DATEADD(YEAR, 5, DATEFROMPARTS(YEAR(CURRENT_DATE), 1, 1)) - 1
+), 
+CalendarDates2 as (
+SELECT
+calendar_date AS DATE
+,YEAR(calendar_date) AS YEAR
+,MONTH(calendar_date) AS MONTH
+,MONTHNAME(calendar_date) AS MONTHNAME
+,DAY(calendar_date) AS DAY
+,DAYNAME(calendar_date) AS DAYNAME
+,QUARTER(calendar_date) AS QUARTER
+,WEEK(calendar_date) AS WEEK
+
+,DAYOFMONTH(calendar_date) AS DAYOFMONTH
+,DAYOFWEEK(calendar_date) AS DAYOFWEEK
+,DAYOFYEAR(calendar_date) AS DAYOFYEAR
+
+,LAST_DAY (calendar_date) AS LAST_DAY_OF_MONTH
+,LAST_DAY (calendar_date, 'QUARTER') AS LAST_DAY_OF_QUARTER
+,LAST_DAY (calendar_date, 'YEAR') AS LAST_DAY_OF_YEAR
+,LAST_DAY (calendar_date, 'WEEK') AS LAST_DAY_OF_WEEK
+
+,WEEKOFYEAR(calendar_date) AS WEEKOFYEAR
+,YEAROFWEEK(calendar_date) AS YEAROFWEEK
+,YEAROFWEEKISO(calendar_date) AS YEAROFWEEKISO
+,WEEKISO(calendar_date) AS WEEKISO
+,DAYOFWEEKISO(calendar_date) AS DAYOFWEEKISO
+
+,CEIL(DAYOFMONTH(calendar_date)/7) AS WEEK_OF_MONTH
+
+,CASE
+WHEN QUARTER(calendar_date) = 1 THEN DATEFROMPARTS(YEAR(calendar_date), 1, 1)
+WHEN QUARTER(calendar_date) = 2 THEN DATEFROMPARTS(YEAR(calendar_date), 4, 1)
+WHEN QUARTER(calendar_date) = 3 THEN DATEFROMPARTS(YEAR(calendar_date), 7, 1)
+ELSE DATEFROMPARTS(YEAR(calendar_date), 10, 1)
+END AS FIRST_DAY_OF_QUARTER
+,DATEDIFF (DAY, FIRST_DAY_OF_QUARTER, DATE) + 1 AS DAY_OF_QUARTER
+,DATEDIFF (DAY, FIRST_DAY_OF_QUARTER, LAST_DAY_OF_QUARTER) + 1 AS DAYS_IN_QUARTER
+,CEIL(DAY_OF_QUARTER/7) AS WEEK_OF_QUARTER
+FROM CalendarDates
+order by year asc
+)
+
+select * from CalendarDates2
+;
+
+select * from lf_wh_db.common.dim_time;
